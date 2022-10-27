027650ecd1e1a6517c6d94cd248da2c7a332e228
michielvanagtmaal <madlog2912@gmail.com>
2022-10-27 15:21:47 +0200

    feat: add sky

--- lab4 - threejs/main.js
+++ lab4 - threejs/main.js
@@ -3,10 +3,13 @@ import * as THREE from 'three';
 
 
 
-const scene = new THREE.Scene();
+      const scene = new THREE.Scene();
 			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
 			const renderer = new THREE.WebGLRenderer();
+      const loader = new THREE.TextureLoader();
+      scene.background = loader.load("/assets/textures/sky.webp");
 			renderer.setSize( window.innerWidth, window.innerHeight );
 			document.body.appendChild( renderer.domElement );
 
