dc7cf914e58d5dda8f88b56bf9a1a2b586de65cb
michielvanagtmaal <madlog2912@gmail.com>
2022-10-27 16:19:35 +0200

    feat: add floor

--- lab4 - threejs/main.js
+++ lab4 - threejs/main.js
@@ -1,32 +1,68 @@
-import './style.css';
+import './style.css'
+import javascriptLogo from './javascript.svg'
+//import three
 import * as THREE from 'three';
+//import orbit controls
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Material } from 'three';
+// import gltf loader
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// add directional light
+const light = new THREE.DirectionalLight( 0xffffff, 1 );
+light.position.set( 0, 1, 1 ).normalize();
+scene.add( light );
+
+
+//ORBIT CONTROLS
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
+// texture loader
+const textureloaderGrass = new THREE.TextureLoader();
+const grassTexture = textureloaderGrass.load('/assets/textures/sand.jpeg');
 
 
 
-      const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 
-			const renderer = new THREE.WebGLRenderer();
-      const loader = new THREE.TextureLoader();
-      scene.background = loader.load("/assets/textures/sky.jpeg");
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
 
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
 
-			camera.position.z = 5;
 
-			function animate() {
-				requestAnimationFrame( animate );
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
 
-				renderer.render( scene, camera );
-			};
 
-			animate();
+
+// create a box
+const grass = new THREE.BoxGeometry(10, 10, 0.1);
+const grassMaterial = new THREE.MeshBasicMaterial( { color: 0xaaaaaa } );
+grassMaterial.map = grassTexture;
+const grassMesh = new THREE.Mesh( grass, grassMaterial );
+// rotate the box
+grassMesh.rotation.x = Math.PI/2;
+// move the box
+grassMesh.position.y = -1;
+scene.add( grassMesh );
+
+
+
+
+camera.position.z = 5;
+
+function animate() {
+  camera.position.x = 2;
+  camera.position.y = 1.01;
+
+  requestAnimationFrame( animate );
+  controls.update();
+	renderer.render( scene, camera );
+};
+
+	animate();
